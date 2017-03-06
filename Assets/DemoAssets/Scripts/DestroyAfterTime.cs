88b7600736b54736e0f1c3dd4c4508bbee3f4d10
mtytel <matthewtytel@gmail.com>
2017-03-06 15:01:52 -0500

    Using prefabs in demos and repetitive spawner in 2d music demo

---
+++ Assets/DemoAssets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Tytel {
+    public class DestroyAfterTime : MonoBehaviour {
+
+        public float time = 10.0f;
+
+        void Start() {
+            Invoke("Die", time);
+        }
+
+        void Die() {
+            Destroy(gameObject);
+        }
+    }
+}
