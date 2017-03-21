109f3b4e8b50d021a583c20926d678665885627f
Matt Tytel <matthewtytel@gmail.com>
2017-03-21 00:55:05 -0400

    Moved mixers. Bringing in sequencers in order.

---
+++ Assets/Helm/Demos/Assets/Scripts/EnableAfterTime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Tytel
+{
+    public class EnableAfterTime : MonoBehaviour
+    {
+        public float time = 1.0f;
+        public MonoBehaviour behavior;
+
+        void Start()
+        {
+            Invoke("Enable", time);
+        }
+
+        void Enable()
+        {
+            behavior.enabled = true;
+        }
+    }
+}
