ebfdc926152a634966b6fbeac1db9bab36747e34
Matt Tytel <matthewtytel@gmail.com>
2017-07-10 18:35:13 -0400

    Make sure we are building to Helm directory.

--- NativeCode/build_android.sh
+++ NativeCode/build_android.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 $ANDROID_NDK_ROOT/ndk-build NDK_PROJECT_PATH=. NDK_APPLICATION_MK=Application.mk $*
-mkdir -p ../Assets/Plugins/Android/
-rm -rf ../Assets/Plugins/Android/libs
-mv libs ../Assets/Plugins/Android/libs
+mkdir -p ../Assets/Helm/Plugins/Android/
+rm -rf ../Assets/Helm/Plugins/Android/libs
+mv libs ../Assets/Helm/Plugins/Android/libs
 
 echo ""
 echo "cleaning up libs"
