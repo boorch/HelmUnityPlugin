1ef81a2f37957d261720692711a8b3d026266987
mtytel <matthewtytel@gmail.com>
2017-10-14 09:54:20 -0700

    Added example for hooking into sequencer events.

--- Assets/AudioHelm/Demos/Assets/Scripts/MaterialPulse.cs
+++ Assets/AudioHelm/Demos/Assets/Scripts/MaterialPulse.cs
@@ -1,22 +1,26 @@
 ﻿// Copyright 2017 Matt Tytel
 
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace AudioHelm
 {
     [AddComponentMenu("")]
     public class MaterialPulse : MonoBehaviour
     {
-        public Renderer render;
         public Material defaultMaterial;
         public Material pulseMaterial;
         public float decay = 2.0f;
 
         float progress = 0.0f;
+        Renderer render;
+        RawImage rawImage;
 
         public void Pulse(float amount)
         {
             progress = Mathf.Max(progress, Mathf.Clamp(amount, 0.0f, 1.0f));
+            render = GetComponent<Renderer>();
+            rawImage = GetComponent<RawImage>();
         }
 
         void Update()
@@ -26,7 +30,10 @@ namespace AudioHelm
 
             float t = Mathf.Clamp(decay * Time.deltaTime, 0.0f, 1.0f);
             progress = Mathf.Lerp(progress, 0.0f, t);
-            render.material.Lerp(defaultMaterial, pulseMaterial, progress);
+            if (render)
+                render.material.Lerp(defaultMaterial, pulseMaterial, progress);
+            if (rawImage)
+                rawImage.color = Color.Lerp(defaultMaterial.color, pulseMaterial.color, progress);
         }
     }
 }
